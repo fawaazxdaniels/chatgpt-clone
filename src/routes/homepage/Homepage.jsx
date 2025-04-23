256bccea343ecfc7627c50f216b09f401c466387
fawaazxdaniels <fawaazxdaniels@gmail.com>
2025-04-23 17:43:10 +0200

    modified home page

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -4,7 +4,21 @@ import './homepage.css';
 const Homepage = () => {
   return (
     <div className='homepage'>
-      <Link to ="/dashboard">Dashboard</Link>
+      <img src="/orbital.png" alt="" className="orbital"/>
+      <div className="left">
+        <h1>LAMA AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, dsaibc asdnfiiucasd jdasjibixa adiabcbsdib.</h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot"/>
+        </div>
+      </div>
     </div>
   );
 };
